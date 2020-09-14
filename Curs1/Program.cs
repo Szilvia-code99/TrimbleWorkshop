78ade5b0ef309ba1912a5866b41b2ad87517db62
Szilvia <szilviakovacs55@gmail.com>
2020-09-14 14:32:50 +0300

    Add project files.

---
+++ Curs1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Curs1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Please enter the radius of the circle");
+           int radius= Convert.ToInt32(Console.ReadLine());
+           double area = Math.PI * radius * radius;
+           Console.WriteLine("The area of the circle is "+area);
+         //  Console.ReadKey();
+
+
+        }
+    }
+}
