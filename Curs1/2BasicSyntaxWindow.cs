f235ed7d16ee99014c51b446fd654e2b25deae10
Szilvia <szilviakovacs55@gmail.com>
2020-09-14 18:32:36 +0300

    Tema 1,2,3 Surface of triangle, celsius to kelvin and fahrnheit, calculate triangle from angles

---
+++ Curs1/2BasicSyntaxWindow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Curs1
+{
+    // class keyword used for declaration
+    class BasicSyntaxWindow
+    {
+        // member variables used for storing data
+        int height;
+        int width;
+
+        // member function
+        public void SetDimensions()
+        {
+            height = 3;
+            width = 2;
+        }
+
+        // member function
+        public int GetWindowArea()
+        {
+            return height * width;
+        }
+
+        // member function
+        public void Display()
+        {
+            Console.WriteLine($"Height: {height}");
+            Console.WriteLine($"Width: {width}");
+            Console.WriteLine($"Area: {GetWindowArea()}");
+        }
+    }
+}
