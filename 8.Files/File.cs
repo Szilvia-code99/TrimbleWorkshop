a64bd431bafc9e037687ffbb025bc77901635546
Szilvia <szilviakovacs55@gmail.com>
2020-09-21 08:17:52 +0300

    adding football scores to txt file then printing statistics form it(wins,draws,losses,etc.)

--- 8.Files/File.cs
+++ 8.Files/File.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
 
 namespace Files
 {
@@ -36,9 +37,9 @@ namespace Files
 
       }
 
-    /// <summary>
-    /// method for finding a file and copying its content to another file whilst convertin every character to uppercase
-    /// </summary>
+      /// <summary>
+      /// method for finding a file and copying its content to another file whilst convertin every character to uppercase
+      /// </summary>
       public static void FindFile()
       {
          string oldFile = string.Empty;
@@ -60,51 +61,100 @@ namespace Files
             while (line != null);
             fileRw.Close();
             fileWr.Close();
+         } else {
+            Console.WriteLine("File DOES NOT exists");
          }
       }
-     
 
-      static void Main(string[] args)
+      /// <summary>
+      /// Filling a txt with user input of scores
+      /// </summary>
+      public static void AddFootballScores()
       {
-         List<Teacher> teachers = new List<Teacher> {
+         using (System.IO.StreamWriter file =
+           new System.IO.StreamWriter(@"C:\\Users\\Szilvia\\Desktop\\Trimble2020\\TrimbleWorkshop\\football.txt", true)) {
+            string exit = string.Empty;
+            string score = string.Empty;
+            do {
+               UserInputValidator.UserInputValidation.ValidateUserInput(ref score, "Type the score like (x-y)");
+               file.WriteLine(score);
+               do {
+                  UserInputValidator.UserInputValidation.ValidateUserInput(ref exit, "Do you want to add more scores?Y/N");
+               } while (exit != "Y" && exit != "N");
+            } while (exit == "Y");
+         }
+      }
+      /// <summary>
+      /// Printing statistics of football team from file(wins/draws/losses/points/numberOfPoints(count with 3p for W, 1p for D, 0p for L)
+      /// </summary>
+      public static void PrintStatisticsOfFootballTeam()
+      {
+         int wins = 0, draws = 0, losses = 0, points = 0, numberOfPoints = 0;
+         using (System.IO.StreamReader fileRw =
+          new System.IO.StreamReader(@"C:\\Users\\Szilvia\\Desktop\\Trimble2020\\TrimbleWorkshop\\football.txt", true)) {
+            string line;
+            while ((line = fileRw.ReadLine()) != null) {
+               string[] scores = line.Split('-');
+                 if (Int32.Parse(scores[0]) > Int32.Parse(scores[1])) {
+                  wins++;
+                  points += Int32.Parse(scores[0]);
+               } else if (Int32.Parse(scores[0]) < Int32.Parse(scores[1])) {
+                  losses++;
+                  points += Int32.Parse(scores[0]);
+               } else if (Int32.Parse(scores[0]) == Int32.Parse(scores[1])) {
+                  draws++;
+                  points += Int32.Parse(scores[0]);
+               }
+            }
+         }
+         numberOfPoints = wins * 3 + draws;
+         Console.WriteLine($"Wins: {wins}, Draws: {draws}, Losses: {losses}, Points: {points}, NumberOfPoints: {numberOfPoints}");
+      }
+         static void Main(string[] args)
+         {
+            List<Teacher> teachers = new List<Teacher> {
             new Teacher(){name="Anne",surname="Marie",age=30,identifier=83262,workingHours=5,subject="Biology"},
             new Teacher(){name="Kate",surname="Winslet",age=25,identifier=938274,workingHours=8,subject="Mathematics" }
           };
 
-         List<Student> students = new List<Student> {
+            List<Student> students = new List<Student> {
             new Student(){ name = "Anna", surname = "Bela",age= 45,identifier= 38373,yearOfStudy=1,studyProgram= "oodhf" },
             new Student() { name="Viktor",surname= "Orban",age= 25,identifier= 938274,yearOfStudy= 3,studyProgram= "Politics" },
             new Student(){name="Ion", surname="Cruza",age= 26, identifier=83737832,yearOfStudy=4,studyProgram= "Architecture" },
             new Student(){name="Jennifer",surname= "Aniston",age =40, identifier=1983262,yearOfStudy=6,studyProgram= "Drama" }
           };
 
-        
-         //1.
-         SerializeToXml(students, "C:\\Users\\Szilvia\\Desktop\\Trimble2020\\TrimbleWorkshop\\lists.xml");
-         XmlSerializer serializer1 = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("ArrayOfStudent"));
-         StreamReader stReader = new StreamReader("C:\\Users\\Szilvia\\Desktop\\Trimble2020\\TrimbleWorkshop\\lists.xml");
-         //2.
-         List<Student>st = (List<Student>)serializer1.Deserialize(stReader);
-         stReader.Close();
-         SerializeToXml(teachers, "C:\\Users\\Szilvia\\Desktop\\Trimble2020\\TrimbleWorkshop\\lists.xml");
-         XmlSerializer serializer2 = new XmlSerializer(typeof(List<Teacher>), new XmlRootAttribute("ArrayOfTeacher"));
-         StreamReader strReader = new StreamReader("C:\\Users\\Szilvia\\Desktop\\Trimble2020\\TrimbleWorkshop\\lists.xml");
-         List<Teacher> t = (List<Teacher>)serializer2.Deserialize(strReader);
 
-         //1.
-         JsonSerializer serializer3 = new JsonSerializer();
-         using StreamWriter sw = new StreamWriter(@"C:\\Users\\Szilvia\\Desktop\\Trimble2020\\TrimbleWorkshop\\lists2.json");
-         using JsonWriter writer = new JsonTextWriter(sw);
-         serializer3.Serialize(writer, students);
-         sw.Close();
-         writer.Close();
+            //1.
+            SerializeToXml(students, "C:\\Users\\Szilvia\\Desktop\\Trimble2020\\TrimbleWorkshop\\lists.xml");
+            XmlSerializer serializer1 = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("ArrayOfStudent"));
+            StreamReader stReader = new StreamReader("C:\\Users\\Szilvia\\Desktop\\Trimble2020\\TrimbleWorkshop\\lists.xml");
+            //2.
+            List<Student> st = (List<Student>)serializer1.Deserialize(stReader);
+            stReader.Close();
+            SerializeToXml(teachers, "C:\\Users\\Szilvia\\Desktop\\Trimble2020\\TrimbleWorkshop\\lists.xml");
+            XmlSerializer serializer2 = new XmlSerializer(typeof(List<Teacher>), new XmlRootAttribute("ArrayOfTeacher"));
+            StreamReader strReader = new StreamReader("C:\\Users\\Szilvia\\Desktop\\Trimble2020\\TrimbleWorkshop\\lists.xml");
+            List<Teacher> t = (List<Teacher>)serializer2.Deserialize(strReader);
 
-         //2.
-        // StreamReader streamReader = new StreamReader("C:\\Users\\Szilvia\\Desktop\\Trimble2020\\TrimbleWorkshop\\lists2.json");
-        // List<Student> sss=JsonConvert.DeserializeObject<List<Student>>("C:\\Users\\Szilvia\\Desktop\\Trimble2020\\TrimbleWorkshop\\lists2.json");
+            //1.
+            JsonSerializer serializer3 = new JsonSerializer();
+            using StreamWriter sw = new StreamWriter(@"C:\\Users\\Szilvia\\Desktop\\Trimble2020\\TrimbleWorkshop\\lists2.json");
+            using JsonWriter writer = new JsonTextWriter(sw);
+            serializer3.Serialize(writer, students);
+            sw.Close();
+            writer.Close();
 
-         //3.
-         FindFile();
+            //2.
+            // StreamReader streamReader = new StreamReader("C:\\Users\\Szilvia\\Desktop\\Trimble2020\\TrimbleWorkshop\\lists2.json");
+            // List<Student> sss=JsonConvert.DeserializeObject<List<Student>>("C:\\Users\\Szilvia\\Desktop\\Trimble2020\\TrimbleWorkshop\\lists2.json");
+
+            //3.
+            FindFile();
+
+         //4.
+         AddFootballScores();
+         PrintStatisticsOfFootballTeam();
+         }
       }
    }
-}
